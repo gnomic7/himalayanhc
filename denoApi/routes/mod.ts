c6204fbd633011942fbff130bc7d4a4fc01cd30c
rohit <gnomic7@gmail.com>
2022-06-21 20:43:09 -0400

    Fix cors

--- denoApi/routes/mod.ts
+++ denoApi/routes/mod.ts
@@ -5,29 +5,25 @@ import { sendEmail } from '../helpers/mod.ts';
 import { validatePayload, packageResponsePayload } from '../utils/mod.ts';
 
 const router = new Router();
-router.post(
-  '/hhc',
-  oakCors({ origin: () => ['https://himalayanhc.github.io/'] }),
-  async (context) => {
-    let resp;
-    try {
-      const payloadStringified = await context.request.body().value;
-      const payload = JSON.parse(payloadStringified);
-      const { error, isPayloadValid } = validatePayload(payload);
-      if (isPayloadValid) {
-        const { error: err, message } = await sendEmail(payload);
-        if (err) throw new Error(err);
-        resp = packageResponsePayload(message || 'success', 200);
-      } else {
-        resp = packageResponsePayload(error, 422);
-      }
-    } catch (allError) {
-      resp = packageResponsePayload(allError.message, 500);
+router.post('/hhc', oakCors(), async (context) => {
+  let resp;
+  try {
+    const payloadStringified = await context.request.body().value;
+    const payload = JSON.parse(payloadStringified);
+    const { error, isPayloadValid } = validatePayload(payload);
+    if (isPayloadValid) {
+      const { error: err, message } = await sendEmail(payload);
+      if (err) throw new Error(err);
+      resp = packageResponsePayload(message || 'success', 200);
+    } else {
+      resp = packageResponsePayload(error, 422);
     }
-    context.response.body = resp.body;
-    context.response.status = resp.status;
-  },
-);
+  } catch (allError) {
+    resp = packageResponsePayload(allError.message, 500);
+  }
+  context.response.body = resp.body;
+  context.response.status = resp.status;
+});
 router.get('/', (context) => {
   context.response.body = 'Muhaaaaaaaaa!';
 });
